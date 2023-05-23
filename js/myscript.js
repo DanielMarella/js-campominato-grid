91c4f722c94e0de667e9cb82dbab888cc5f1c45b
DanielMarella <marella.daniel96@gmail.com>
2023-05-23 16:54:24 +0200

    add click button, but DON'T SPAM

--- js/myscript.js
+++ js/myscript.js
@@ -5,36 +5,36 @@ L'utente clicca su un bottone che genererà una griglia di gioco quadrata. Ogni 
 
 */
 
-
-// Selezione l'elemento a cui aggiungere i nuovi elementi
-const divElement = document.querySelector('main');
-
-// creo una funzione per creare il primo elemento
-function addElement(){
-    const newDivElement = document.createElement('div');
-    newDivElement.classList.add('grid');
-    return newDivElement;
-}
-
-// creo una seconda funzione per inserire il secondo elemento
-function addCellElement(){
-    const newCellElement = document.createElement('div');
-    newCellElement.classList.add('cell');
-    return newCellElement;
-}
-
-
-// scrivo in pagina il primo elemento
-const gridElement = addElement();
-divElement.appendChild(gridElement);
-
-// creo un ciclo che stampi in pagina 100 volte il secondo elemento all'interno del primo
-for (let i = 0; i < 100; i++){
-    const cellElement = addCellElement();
-    gridElement.appendChild(cellElement);
-}
-
-
-
+button.addEventListener('click',
+function(){
+    // Selezione l'elemento a cui aggiungere i nuovi elementi
+    const divElement = document.querySelector('main');
+    
+    // creo una funzione per creare il primo elemento
+    function addElement(){
+        const newDivElement = document.createElement('div');
+        newDivElement.classList.add('grid');
+        return newDivElement;
+    }
+    
+    // creo una seconda funzione per inserire il secondo elemento
+    function addCellElement(){
+        const newCellElement = document.createElement('div');
+        newCellElement.classList.add('cell');
+        return newCellElement;
+    }
+    
+    // scrivo in pagina il primo elemento
+    const gridElement = addElement();
+    divElement.appendChild(gridElement);
+    
+    // creo un ciclo che stampi in pagina 100 volte il secondo elemento all'interno del primo
+    for (let i = 0; i < 100; i++){
+        const cellElement = addCellElement();
+        gridElement.appendChild(cellElement);
+    }
+    return true
+}    
+)
 
 
