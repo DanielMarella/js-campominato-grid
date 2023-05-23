53d05ab98b1db8d72bf3104fd4421697f3621e98
DanielMarella <marella.daniel96@gmail.com>
2023-05-23 16:32:23 +0200

    create the element in js, not done functional way but it work, later i will implement it

--- js/myscript.js
+++ js/myscript.js
@@ -3,18 +3,38 @@
 /* 
 L'utente clicca su un bottone che genererà una griglia di gioco quadrata. Ogni cella ha un numero progressivo, da 1 a 100. Ci saranno quindi 10 caselle per ognuna delle 10 righe. Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
-
-
-Bonus
-Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-Consigli del giorno: :party_wizard:
-Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
-Ad esempio:
-Di cosa ho bisogno per generare i numeri?
-Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
-Le validazioni e i controlli possiamo farli anche in un secondo momento.
-
 */
+
+
+// Selezione l'elemento a cui aggiungere i nuovi elementi
+const divElement = document.querySelector('main');
+
+// creo una funzione per creare il primo elemento
+function addElement(){
+    const newDivElement = document.createElement('div');
+    newDivElement.classList.add('grid');
+    return newDivElement;
+}
+
+// creo una seconda funzione per inserire il secondo elemento
+function addCellElement(){
+    const newCellElement = document.createElement('div');
+    newCellElement.classList.add('cell');
+    return newCellElement;
+}
+
+
+// scrivo in pagina il primo elemento
+const gridElement = addElement();
+divElement.appendChild(gridElement);
+
+// creo un ciclo che stampi in pagina 100 volte il secondo elemento all'interno del primo
+for (let i = 0; i < 100; i++){
+    const cellElement = addCellElement();
+    gridElement.appendChild(cellElement);
+}
+
+
+
+
+
