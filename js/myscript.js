5aadb4f9729d9161deddc1970d23836b3011e163
DanielMarella <marella.daniel96@gmail.com>
2023-05-23 18:22:07 +0200

    button can only press one time

--- js/myscript.js
+++ js/myscript.js
@@ -5,40 +5,42 @@ L'utente clicca su un bottone che genererà una griglia di gioco quadrata. Ogni 
 
 */
 
-button.addEventListener('click',
-function(){
-    // Selezione l'elemento a cui aggiungere i nuovi elementi
-    const divElement = document.querySelector('main');
-    
-    // creo una funzione per creare il primo elemento
-    function addElement(){
-        const newDivElement = document.createElement('div');
-        newDivElement.classList.add('grid');
-        return newDivElement;
-    }
-    
-    // creo una seconda funzione per inserire il secondo elemento
-    function addCellElement(){
-        const newCellElement = document.createElement('div');
-        newCellElement.classList.add('cell');
-        return newCellElement;
-    }
-    
-    // scrivo in pagina il primo elemento
-    const gridElement = addElement();
-    divElement.appendChild(gridElement);
-    
-    // creo un ciclo che stampi in pagina 100 volte il secondo elemento all'interno del primo
-    for (let i = 0; i < 100; i++){
-        const cellElement = addCellElement();
+
+
+    button.addEventListener('click',
+    function(){
+        // Selezione l'elemento a cui aggiungere i nuovi elementi
+        const divElement = document.querySelector('main');
         
-        cellElement.addEventListener('click',
-        function(){
+        // creo una funzione per creare il primo elemento
+        function addElement(){
+            const newDivElement = document.createElement('div');
+            newDivElement.classList.add('grid');
+            return newDivElement;
+        }
+        
+        // creo una seconda funzione per inserire il secondo elemento
+        function addCellElement(){
+            const newCellElement = document.createElement('div');
+            newCellElement.classList.add('cell');
+            return newCellElement;
+        }
+        
+        // scrivo in pagina il primo elemento
+        const gridElement = addElement();
+        divElement.appendChild(gridElement);
+        
+        // creo un ciclo che stampi in pagina 100 volte il secondo elemento all'interno del primo
+        for (let i = 1; i < 101; i++){
+            const cellElement = addCellElement();
+            cellElement.innerHTML = i;
+            cellElement.addEventListener('click',
+            function(){
             cellElement.classList.toggle('select');
         }
         )
         gridElement.appendChild(cellElement);
     }
-}    
+} ,  {once : true}
 )
 
